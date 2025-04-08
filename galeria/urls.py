cd962ee8f2c6c3519080e9e0d933a065976e1d6e
Pedro Imanichi <pedroimanichi@gmail.com>
2025-04-08 16:24:31 -0300

    Linkando banco de dados com o site, e configurando admin

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from galeria.views import index
-from galeria.views import imagem
+from galeria.views import index, imagem
+
 
 urlpatterns = [
         path('', index, name='index'),
-        path('imagem/', imagem, name='imagem'),
+        path('imagem/<int:foto_id>', imagem, name='imagem'),
 ]
